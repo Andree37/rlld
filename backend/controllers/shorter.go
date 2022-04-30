20391511a6dad0d1bf90fce803a0a0339e861bf8
Andre <33375224+Andree37@users.noreply.github.com>
2022-04-30 21:34:58 +0100

    feat: shorten function from id to short url end

--- backend/controllers/shorter.go
+++ backend/controllers/shorter.go
@@ -10,19 +10,18 @@ import (
 	"github.com/andree37/rlld/models"
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type ShorterController struct{}
 
 func (s ShorterController) DoShort(c *gin.Context) {
-
 	var shorter models.Shorter
 	err := c.ShouldBindJSON(&shorter)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	short_url := shorter.Shortens()
 
 	database := db.GetDB()
 	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
@@ -35,7 +34,31 @@ func (s ShorterController) DoShort(c *gin.Context) {
 		return
 	}
 
-	id := res.InsertedID
+	if oid, ok := res.InsertedID.(primitive.ObjectID); ok {
 
-	c.JSON(200, gin.H{"message": short_url, "id": id})
+		short_url, err := shorter.Shortens(oid.Hex())
+		if err != nil {
+			c.JSON(400, gin.H{"error": err.Error()})
+		}
+
+		c.JSON(200, gin.H{"short_url": short_url, "id": oid})
+	} else {
+		c.JSON(400, gin.H{"error": "could not create id"})
+	}
 }
+
+// func (s ShorterController) GetUrlFromId(c *gin.Context) {
+// 	var shorter models.Shorter
+// 	err := c.ShouldBindJSON(&shorter)
+// 	if err != nil {
+// 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+// 		return
+// 	}
+
+// 	database := db.GetDB()
+// 	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+// 	defer cancel()
+
+// 	err = database.Collection("short").FindOne(ctx, filter).Decode(&result)
+
+// }
--- backend/models/shorter.go
+++ backend/models/shorter.go
@@ -1,11 +1,66 @@
 package models
 
+import (
+	"errors"
+	"fmt"
+	"math/big"
+	"strings"
+)
+
 type Shorter struct {
 	Url string `json:"url" binding:"required"`
 }
 
 // Shorter logic here
-func (s *Shorter) Shortens() string {
+func (s *Shorter) Shortens(id string) (string, error) {
 
-	return s.Url + "potato"
+	return idToShortUrl(id)
+}
+
+func idToShortUrl(id string) (string, error) {
+	m := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	shortURL := ""
+
+	// convert id to an integer[]
+	var ints []string
+	for _, c := range id {
+		ints = append(ints, fmt.Sprintf("%v", int(c)))
+	}
+	x := strings.Join(ints, "")
+	integer_id := new(big.Int)
+	integer_id, ok := integer_id.SetString(x, 10)
+	if !ok {
+		return "", errors.New("could not turn string into integer")
+	}
+
+	mod_int := new(big.Int)
+
+	for integer_id.Cmp(big.NewInt(0)) > 0 {
+		shortURL += string(m[mod_int.Mod(integer_id, big.NewInt(62)).Int64()])
+		integer_id.Div(integer_id, big.NewInt(62))
+	}
+
+	//reverse the shortURL
+	result := ""
+	for _, v := range shortURL {
+		result = string(v) + result
+	}
+
+	return result, nil
+}
+
+func shortURLToID(shortURL string) int {
+	id := 0
+	for _, v := range shortURL {
+		val_i := int(v)
+		if val_i >= int('a') && val_i <= int('Z') {
+			id = id*62 + val_i - int('a')
+		} else if val_i >= int('A') && val_i <= int('Z') {
+			id = id*62 + val_i - int('Z') + 26
+		} else {
+			id = id*62 + val_i - int('0') + 52
+		}
+	}
+
+	return id
 }
