51d2dab06e021ce4aa0731679a4a62dcfa243f40
Andre <33375224+Andree37@users.noreply.github.com>
2022-06-01 19:06:20 +0100

    dockerization thingie

--- db/db.go
+++ db/db.go
@@ -21,7 +21,6 @@ func Init() {
 	var err error
 	psqlconn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
 	db, err = sql.Open("postgres", psqlconn)
-	fmt.Printf("host name is: %v\n", host)
 	if err != nil {
 		panic(err)
 	}
--- server/router.go
+++ server/router.go
@@ -1,30 +1,42 @@
 package server
 
 import (
-	"fmt"
 	"github.com/andree37/rlld/controllers"
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"os"
 )
 
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
+
 func SetupRouter() *gin.Engine {
 
-	frontend := fmt.Sprintf("%s://%s:%s", os.Getenv("FRONTEND_PROTOCOL"), os.Getenv("FRONTEND_IP"), os.Getenv("FRONTEND_PORT"))
+	//frontend := fmt.Sprintf("%s://%s:%s", os.Getenv("FRONTEND_PROTOCOL"), os.Getenv("FRONTEND_IP"), os.Getenv("FRONTEND_PORT"))
 
 	// mode of the release
 	gin.SetMode(os.Getenv("RELEASE_MODE"))
 
 	router := gin.Default()
+
 	err := router.SetTrustedProxies(nil)
 	if err != nil {
 		return nil
 	}
-	// add here the frontend ip
-	corsConfig := cors.DefaultConfig()
-	corsConfig.AllowOrigins = []string{frontend}
 
-	router.Use(cors.New(corsConfig))
+	router.Use(CORSMiddleware())
 	// init controllers
 	url := new(controllers.URLController)
 
