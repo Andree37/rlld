eb51df8281268995864fa6d51e33ea7ad0a36799
Andre Ribeiro <andresmribeiro@gmail.com>
2023-05-23 16:30:55 +0100

    env mby?

--- main.go
+++ main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"database/sql"
+
 	"github.com/andree37/rlld/db"
 	"github.com/andree37/rlld/server"
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	err := godotenv.Load(".env")
+	err := godotenv.Load()
 	if err != nil {
 		panic("Something went wrong with the .env")
 	}
@@ -21,7 +22,6 @@ func main() {
 	defer func(database *sql.DB) {
 		err := database.Close()
 		if err != nil {
-
 		}
 	}(database)
 }
